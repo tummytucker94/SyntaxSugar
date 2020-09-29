2690547ba3bd6ccd122aa15040e47c029241966c
Jermaine Tucker <jtuck1623@gmail.com>
2020-09-29 17:01:45 -0400

    refining sugar

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace SyntaxSugar
         {
             var answer = 4;
             var response = (answer > 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+            Console.WriteLine(response);
         }
     }
 }
