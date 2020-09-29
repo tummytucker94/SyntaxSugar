94e5aacbab6fcba173ecfe5d21e8061008429931
Jermaine Tucker <jtuck1623@gmail.com>
2020-09-29 16:59:34 -0400

    adding sugar

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer > 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+        }
+    }
+}
